0db50b3adfc799f72c5c8477fba1bf5ee6bcd835
paulmlaviwa <mlaviwa.paul@gmail.com>
2023-10-16 12:16:02 -0400

    Error handling tested

---
+++ src/main/java/com/pluralsight/FamousQuotes.java
@@ -0,0 +1,34 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class FamousQuotes {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Choose a number (between 1 - 10) of a quote: ");
+
+        try {
+            int quoteNumber = myScanner.nextInt();
+
+            String[] famousQuotes = {
+                    "The greatest glory in living lies not in never falling, but in rising every time we fall. -Nelson Mandela",
+                    "The way to get started is to quit talking and begin doing. -Walt Disney",
+                    "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking. -Steve Jobs",
+                    "The future belongs to those who believe in the beauty of their dreams. -Eleanor Roosevelt",
+                    "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough. -Oprah Winfrey",
+                    "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success. -James Cameron",
+                    "You may say I'm a dreamer, but I'm not the only one. I hope someday you'll join us. And the world will live as one. -John Lennon",
+                    "You must be the change you wish to see in the world. -Mahatma Gandhi",
+                    "Spread love everywhere you go. Let no one ever come to you without leaving happier. -Mother Teresa",
+                    "The only thing we have to fear is fear itself. -Franklin D. Roosevelt"
+
+            };
+            int index = quoteNumber - 1;
+
+            String selectedQuote = famousQuotes[index];
+            System.out.println("You chose: " + selectedQuote);
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
+    }
+}
